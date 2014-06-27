3528c8eff3164801d320830138d44e8896e0725c
Guilherme Barbosa Ferreira <gimoteco@gmail.com>
2014-06-27 08:14:05 -0400

    Inclusão dos arquivos ausentes

---
+++ EscritorDeNumeroPorExtenso.Testes/TesteDeCentavos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using EscritorDeNumeroPorExtenso.ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EscritorDeNumeroPorExtenso.Testes
+{
+    [TestClass]
+    public class TesteDeCentavos
+    {
+        [TestMethod]
+        public void DeveGerarCinquentaCentesimos()
+        {
+            Assert.AreEqual("dez centésimos", new Centesimos(new Dezena(1)).ToString());
+        }
+
+        [TestMethod]
+        public void DeveGerarUmMilhaoECinquentaCentesimos()
+        {
+            Assert.AreEqual("um milhão e cinquenta centésimos", new Milhao(new Unidade(1), new Centesimos(new Dezena(5))).ToString());
+        }
+
+        [TestMethod]
+        public void DeveGerarCentesimoNoSingular()
+        {
+            Assert.AreEqual("um centésimo", new Centesimos(new Unidade(1)).ToString());
+        }
+    }
+}
---
+++ EscritorDeNumeroPorExtenso.Testes/TesteDeMilhao.cs
@@ -0,0 +1,33 @@
+using EscritorDeNumeroPorExtenso.ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EscritorDeNumeroPorExtenso.Testes
+{
+    [TestClass]
+    public class TesteDeMilhao
+    {
+        [TestMethod]
+        public void DeveGerarNumeroUmMilhao()
+        {
+            Assert.AreEqual("um milhão", new Milhao(new Unidade(1)).ToString());
+        }
+
+        [TestMethod]
+        public void DeveGerarNumeroDezMilhões()
+        {
+            Assert.AreEqual("dez milhões", new Milhao(new Dezena(1)).ToString());
+        }
+
+        [TestMethod]
+        public void DeveGerarNumeroUmMilhaoECemMil()
+        {
+            Assert.AreEqual("um milhão e cem mil", new Milhao(new Unidade(1), new Milhar(new Centena(1))).ToString());
+        }
+
+        [TestMethod]
+        public void DeveGerarNumeroUmMilhaoECemMilEUm()
+        {
+            Assert.AreEqual("um milhão e cem mil e um", new Milhao(new Unidade(1), new Milhar(new Centena(1), new PrimeiraClasse(new Unidade(1)))).ToString());
+        }
+    }
+}
