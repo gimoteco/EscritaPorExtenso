354e876e7a966e5ce26c7067f792cb47761cce33
Guilherme Barbosa Ferreira <gimoteco@gmail.com>
2015-10-03 13:52:15 -0400

    Correção da codificação do caracter três Preferências do usuario do monodevelop ignorado

--- EscritaPorExtenso.Lib/Core/Unidade.cs
+++ EscritaPorExtenso.Lib/Core/Unidade.cs
@@ -8,7 +8,7 @@ namespace EscritaPorExtenso.Lib
 
         private static readonly Dictionary<int, string> _nomeDosAlgarismos = new Dictionary<int, string>()
         {
-                {0, "zero"}, {1, "um"}, {2, "dois"}, {3, "três"},
+                {0, "zero"}, {1, "um"}, {2, "dois"}, {3, "tr¨ºs"},
                 {4, "quatro"}, {5, "cinco"}, {6, "seis"},
                 {7, "sete"}, {8, "oito"}, {9, "nove"},
         };
