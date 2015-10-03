de3161488c6b6c31e61914da216a81cacc793897
Guilherme Barbosa Ferreira <gimoteco@gmail.com>
2015-10-03 10:33:41 -0400

    Melhoria na organização do projeto Implementações da escrita por extenso de reais

---
+++ EscritaPorExtenso.Lib/Moeda/Real.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace EscritaPorExtenso.Lib.Moeda
+{
+    public class Real
+    {
+        IClasse _parteInteira;
+        IClasse _parteDecimal;
+        private string _pluralidadeInteira;
+        private string _pluralidadeDecimal;
+
+        public Real(double valor)
+        {
+            var numeroDaParteInteira = (int)Math.Truncate(valor);
+            _parteInteira = ConversorDeNumeroParaClasses.Converter(numeroDaParteInteira);
+            _pluralidadeInteira = ObterPluralidadeDaParteInteira(numeroDaParteInteira);
+            var numeroDaParteDecimal = (int) ((valor - Math.Truncate(valor)) * 100);
+
+            if (numeroDaParteDecimal > 0)
+            {
+                _parteDecimal = ConversorDeNumeroParaClasses.Converter(numeroDaParteDecimal);
+                _pluralidadeDecimal = ObterPluralidadeDaParteDecimal(numeroDaParteDecimal);
+            }
+        }
+
+        private static string ObterPluralidadeDaParteInteira(int parteInteira)
+        {
+            return parteInteira > 1 ? "reais" : "real";
+        }
+
+        private static string ObterPluralidadeDaParteDecimal(int numeroDaParteDecimal)
+        {
+            return numeroDaParteDecimal > 1 ? "centavos" : "centavo";
+        }
+
+        public override string ToString()
+        {
+            var parteInteira = string.Format("{0} {1}", _parteInteira, _pluralidadeInteira);
+
+            if (_parteDecimal == null)
+                return parteInteira;
+            
+            return string.Format("{0} e {1} {2}", parteInteira, _parteDecimal, _pluralidadeDecimal);
+        }
+    }
+}
---
+++ EscritaPorExtenso.Testes/TesteDeMoeda.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+using EscritaPorExtenso.Lib.Moeda;
+
+namespace EscritaPorExtenso.Testes
+{
+    [TestFixture]
+    public class TesteDeMoeda
+    {
+        [Test]
+        public void DeveEscreverPorExtensoNumeroSingular()
+        {
+            Assert.AreEqual("um real", new Real(1).ToString());
+        }
+
+        [Test]
+        public void DeveEscreverPorExtensoNumeroPlural() 
+        {
+            Assert.AreEqual("dois reais", new Real(2).ToString());
+        }
+
+        [Test]
+        public void DeveEscreverNumeroComCentavoSingular()
+        {
+            Assert.AreEqual("um real e um centavo", new Real(1.01).ToString());
+        }
+
+        [Test]
+        public void DeveEscreverNumeroComCentavoPlural()
+        {
+            Assert.AreEqual("duzentos reais e cinquenta centavos", new Real(200.50).ToString());
+        }
+    }
+}
+
