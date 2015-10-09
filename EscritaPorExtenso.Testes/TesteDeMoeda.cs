cc05d51607958a077af4f2cab7c4d1c01691f3ef
Guilherme Barbosa Ferreira <gimoteco@gmail.com>
2015-10-09 18:12:13 -0400

    Teste de inteiros por extenso

--- EscritaPorExtenso.Testes/TesteDeMoeda.cs
+++ EscritaPorExtenso.Testes/TesteDeMoeda.cs
@@ -6,16 +6,6 @@ using NUnit.Framework;
 namespace EscritaPorExtenso.Testes
 {
     [TestFixture]
-    public class TesteDeInteiro
-    {
-        [Test]
-        public void DeveEscreverUmNumeroInteiro()
-        {
-            Assert.AreEqual("dois mil", 2000.PorExtenso());
-        }
-    }
-
-    [TestFixture]
     public class TesteDeMoeda
     {
         [Test]
