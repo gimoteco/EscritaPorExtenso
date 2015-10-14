2a2981a76e5c0580c6d093e25c1846ba1489cad6
Guilherme Barbosa Ferreira <gimoteco@gmail.com>
2015-10-13 21:00:09 -0400

    Empacotamento da versão v0.1.1

--- EscritaPorExtenso/Properties/AssemblyInfo.cs
+++ EscritaPorExtenso/Properties/AssemblyInfo.cs
@@ -1,11 +1,11 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.0")]
-[assembly: AssemblyInformationalVersion("0.1.0")]
+[assembly: AssemblyVersion("0.1.1")]
+[assembly: AssemblyFileVersion("0.1.1")]
+[assembly: AssemblyInformationalVersion("0.1.1")]
 [assembly: AssemblyTitle("Escrita por extenso")]
-[assembly: AssemblyDescription("Correções nos nomes das API, escrita por extenso de números inteiros")]
+[assembly: AssemblyDescription("Correção na separação de classes numéricas\nTratamentos das exceções na ligação de classes")]
 [assembly: AssemblyCompany("Guilherme Barbosa Ferreira")]
 
 [assembly: InternalsVisibleTo("EscritaPorExtenso.Testes")]
