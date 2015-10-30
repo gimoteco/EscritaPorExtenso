4879994f2940889504a825e8c249244b5b831eac
Guilherme Barbosa Ferreira <gimoteco@gmail.com>
2015-10-30 17:14:40 -0300

    Correções no commit para porcentagem

--- EscritaPorExtenso.Testes/TesteDePorcentagem.cs
+++ EscritaPorExtenso.Testes/TesteDePorcentagem.cs
@@ -5,14 +5,12 @@ using EscritaPorExtenso.Moeda;
 
 namespace EscritaPorExtenso.Testes
 {
+    /*
+     * http://www.migalhas.com.br/Gramatigalhas/10,MI24539,91041-Como+se+le+8347
+    */
     [TestFixture]
     public class TesteDePorcentagem
     {
-        /*
-         * http://www.migalhas.com.br/Gramatigalhas/10,MI24539,91041-Como+se+le+8347
-         */
-
-        [Test]
         [TestCase(35, Result="trinta e cinco por cento")]
         [TestCase(1000, Result="mil por cento")]
         [TestCase(1000000, Result="um milhão por cento")]
@@ -30,7 +28,6 @@ namespace EscritaPorExtenso.Testes
             return valor.PorExtensoDePorcentagem();
         }
        
-        [Test]
         [TestCase(83.47, Result= "oitenta e três vírgula quarenta e sete por cento")]
         [TestCase(0.3, Result= "zero vírgula três por cento")]
         [TestCase(78, Result= "setenta e oito por cento")]
@@ -39,7 +36,6 @@ namespace EscritaPorExtenso.Testes
             return valor.PorExtensoDePorcentagem();
         }
 
-        [Test]
         [TestCase(125.89, Result = "cento e vinte e cinco vírgula oitenta e nove por cento")]
         [TestCase(0.9, Result = "zero vírgula nove por cento")]
         [TestCase(356, Result = "trezentos e cinquenta e seis por cento")]
@@ -54,13 +50,14 @@ namespace EscritaPorExtenso.Testes
             var numeroMaiorQueOMaximo = Convert.ToInt64(new string('9', ConversorDeNumeroParaClasses.NumeroDeClasses * 3 + 1));
 
             var excecao = Assert.Throws<Exception>(() => numeroMaiorQueOMaximo.PorExtensoDePorcentagem());
+
             Assert.AreEqual(string.Format("O valor {0} é maior que o suportado pela biblioteca", numeroMaiorQueOMaximo), excecao.Message);
         }
 
         [Test]
         public void NaoDeveConverterNumeroComMaisDeDuasCasasDecimais()
         {
-            var valor = 0.999;
+            const double valor = 0.999;
 
             var excecao = Assert.Throws<Exception>(() => valor.PorExtensoDePorcentagem());
 
