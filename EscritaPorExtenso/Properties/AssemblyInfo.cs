07a7e1a4527e6a342ccb31df19ec7b7410f321f1
Guilherme Barbosa Ferreira <gimoteco@gmail.com>
2015-11-05 17:54:34 -0300

    Criação da versão 0.2.1

--- EscritaPorExtenso/Properties/AssemblyInfo.cs
+++ EscritaPorExtenso/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("0.2.0")]
-[assembly: AssemblyFileVersion("0.2.0")]
-[assembly: AssemblyInformationalVersion("0.2.0")]
+[assembly: AssemblyVersion("0.2.1")]
+[assembly: AssemblyFileVersion("0.2.1")]
+[assembly: AssemblyInformationalVersion("0.2.1")]
 [assembly: AssemblyTitle("Escrita por extenso")]
 [assembly: AssemblyDescription("Escreva os seus números por extenso")]
 [assembly: AssemblyCompany("Guilherme Barbosa Ferreira")]
