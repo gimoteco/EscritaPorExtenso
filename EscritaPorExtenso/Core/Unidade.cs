74b1f88229cd1da92515795c0e0c33e26573c1e5
Guilherme Barbosa Ferreira <gimoteco@gmail.com>
2015-11-28 15:37:22 -0300

    Correção no enconding da unidade

--- EscritaPorExtenso/Core/Unidade.cs
+++ EscritaPorExtenso/Core/Unidade.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace EscritaPorExtenso.Core
 {
@@ -7,7 +7,7 @@ namespace EscritaPorExtenso.Core
 
         private static readonly Dictionary<int, string> _nomeDosAlgarismos = new Dictionary<int, string>()
         {
-                {0, "zero"}, {1, "um"}, {2, "dois"}, {3, "tr�s"},
+                {0, "zero"}, {1, "um"}, {2, "dois"}, {3, "três"},
                 {4, "quatro"}, {5, "cinco"}, {6, "seis"},
                 {7, "sete"}, {8, "oito"}, {9, "nove"},
         };
